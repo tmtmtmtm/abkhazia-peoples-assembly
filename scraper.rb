402b1929bfde465613a1ec7dcfe1b5ad4029b56a
Tony Bowden <tony@tmtm.com>
2017-01-08 10:43:58 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'nokogiri'
 require 'pry'
@@ -10,7 +11,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -22,17 +23,17 @@ def scrape_list(url)
   noko = noko_for(url)
   noko.xpath('//div[@class="news-list"]//td[.//h2]').each do |td|
     person_link = td.xpath('.//a[h2]/@href').text
-    data = { 
-      id: person_link[/ELEMENT_ID=(\d+)/, 1],
-      name: td.css('h2').text.tidy,
+    data = {
+      id:     person_link[/ELEMENT_ID=(\d+)/, 1],
+      name:   td.css('h2').text.tidy,
       source: person_link,
-      area: td.text[/Округ №\s*(\d+)/, 1],
-      email: td.xpath('.//small[contains(.,"почта")]').text.tidy.split(/:\s*/, 2).last,
-      image: td.parent.css('img.preview_picture/@src').text,
-      term: 5
+      area:   td.text[/Округ №\s*(\d+)/, 1],
+      email:  td.xpath('.//small[contains(.,"почта")]').text.tidy.split(/:\s*/, 2).last,
+      image:  td.parent.css('img.preview_picture/@src').text,
+      term:   5,
     }
     %i(image source).each { |i| data[i] = URI.join(url, URI.escape(data[i])).to_s unless data[i].to_s.empty? }
-    ScraperWiki.save_sqlite([:id, :term], data)
+    ScraperWiki.save_sqlite(%i(id term), data)
   end
 end
 
